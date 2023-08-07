ed225647f654df64f7d60c0af0e2ac3e501d36cc
fauzan-neos <afauzan0311@gmail.com>
2023-08-07 09:55:25 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test remote</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium eius natus placeat quis, sit repellendus modi ratione labore! Voluptatibus consequuntur dolorum facilis tempore, deleniti officiis. Fugiat debitis omnis optio.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
+</body>
+</html>
